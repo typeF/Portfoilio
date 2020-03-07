9eb60c969ac7fbfc885e84b922f0eca812417b4c
Frank Lam <frank.is.missing@gmail.com>
2020-03-07 15:59:55 -0500

    Added GA plugin to top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-import AboutTemplate from "../templates/aboutTemplate";
+// import AboutTemplate from "../templates/aboutTemplate";
 import ContactTemplate from "../templates/contactTemplate";
 import HeroTemplate from "../templates/heroTemplate";
 import ProjectTemplate from "../templates/projectTemplate";
